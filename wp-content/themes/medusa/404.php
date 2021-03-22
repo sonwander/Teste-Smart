6227f398ab29cd782903387255b09f9905b7f989
sonwander <sonwander@yahoo.com.br>
2021-03-22 10:49:48 -0300

    Update 404.php

--- wp-content/themes/medusa/404.php
+++ wp-content/themes/medusa/404.php
@@ -3,15 +3,15 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main class="site-main">
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'medusa' ); ?></h1>
+				<h1 class="page-title"><?php esc_html_e( 'Nada encontrado.', 'medusa' ); ?></h1>
 			</header>
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'medusa' ); ?></p>
+				<p><?php esc_html_e( 'Nada encontrado aqui. Que tal fazer uma busca?', 'medusa' ); ?></p>
 
 					<?php
 					get_search_form();
@@ -19,30 +19,6 @@ get_header();
 					the_widget( 'WP_Widget_Recent_Posts' );
 					?>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'medusa' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div>
-
-					<?php
-					
-					$medusa_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'medusa' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$medusa_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
 
 			</div>
 		</section>
