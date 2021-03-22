ca1bd57770f17ab8a933b678c8950bce89d5ec91
sonwander <sonwander@yahoo.com.br>
2021-03-22 10:29:05 -0300

    Update archive.php

--- wp-content/themes/medusa/archive.php
+++ wp-content/themes/medusa/archive.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * The template for displaying archive pages
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package MEDUSA
- */
 
 get_header();
 ?>
@@ -19,18 +12,12 @@ get_header();
 				the_archive_title( '<h1 class="page-title">', '</h1>' );
 				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
-			</header><!-- .page-header -->
+			</header>
 
 			<?php
-			/* Start the Loop */
+			
 			while ( have_posts() ) :
 				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
 				get_template_part( 'template-parts/content', get_post_type() );
 
 			endwhile;
@@ -44,7 +31,7 @@ get_header();
 		endif;
 		?>
 
-	</main><!-- #main -->
+	</main>
 
 <?php
 get_sidebar();
