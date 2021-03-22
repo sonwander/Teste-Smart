ba0f74fafb84b3e28097e09cd98eaba5478d82d9
sonwander <sonwander@yahoo.com.br>
2021-03-22 11:31:45 -0300

    Delete archive.php

--- wp-content/themes/medusa/archive.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-<div class="main-content">
-	<?php get_template_part('template_parts/section-top-home'); ?>
-	<?php get_template_part('template_parts/section-middle-home'); ?>
-	<?php get_template_part('template_parts/section-bottom-home'); ?>
-
-</div>
-<?php
-get_footer();
